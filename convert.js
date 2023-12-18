90d495131c3bd5e1284fbd247002239fe38d08b4
仓鼠 <zhuxx2003@gmail.com>
2023-12-18 15:36:13 +0800

    exported 2022

--- convert.js
+++ convert.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
-const { Strings: o } = require('./exported.json')
 
-let res = o.map((v,i)=>({key: i.toString(), original: v}))
-res = JSON.stringify(res, null, 2)
-fs.writeFileSync(`paratranz.json`, res)
+for (let path of ['2022', '2023']) {
+  let { Strings: o } = JSON.parse(fs.readFileSync(`${path}/exported.json`, 'utf-8'))
+  let res = o.map((v, i) => ({ key: i.toString(), original: v }))
+  res = JSON.stringify(res, null, 2)
+  fs.writeFileSync(`${path}/paratranz.json`, res)
+}
